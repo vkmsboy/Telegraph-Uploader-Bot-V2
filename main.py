dab6ac0242e815e1a5daa94ae3629f6acc708d84
Fayas Noushad <76828314+FayasNoushad@users.noreply.github.com>
2022-04-20 13:15:03 +0530

    Update

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import traceback
 import asyncio
 import datetime
 import aiofiles
+from dotenv import load_dotenv
 from random import choice 
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
@@ -17,9 +18,10 @@ from telegraph import upload_file
 from database import Database
 
 
+load_dotenv()
 BOT_OWNER = int(os.environ.get("BOT_OWNER"))
 DATABASE_URL = os.environ.get("DATABASE_URL")
-db = Database(DATABASE_URL, "TelegraphBot")
+db = Database(DATABASE_URL, "Telegraph-Uploader-Bot")
 
 Bot = Client(
     "Telegraph Uploader Bot V2",
@@ -66,7 +68,7 @@ ABOUT_TEXT = """--**About Me**-- 😎
 
 START_BUTTONS = InlineKeyboardMarkup(
     [
-	[
+        [
             InlineKeyboardButton('⚙ Help', callback_data='help'),
             InlineKeyboardButton('About 🔰', callback_data='about'),
             InlineKeyboardButton('Close ✖️', callback_data='close')
@@ -76,7 +78,7 @@ START_BUTTONS = InlineKeyboardMarkup(
 
 HELP_BUTTONS = InlineKeyboardMarkup(
     [
-	[
+        [
             InlineKeyboardButton('🏘 Home', callback_data='home'),
             InlineKeyboardButton('About 🔰', callback_data='about'),
             InlineKeyboardButton('Close ✖️', callback_data='close')
@@ -86,7 +88,7 @@ HELP_BUTTONS = InlineKeyboardMarkup(
 
 ABOUT_BUTTONS = InlineKeyboardMarkup(
     [
-	[
+        [
             InlineKeyboardButton('🏘 Home', callback_data='home'),
             InlineKeyboardButton('Help ⚙', callback_data='help'),
             InlineKeyboardButton('Close ✖️', callback_data='close')
@@ -96,20 +98,20 @@ ABOUT_BUTTONS = InlineKeyboardMarkup(
 
 
 async def send_msg(user_id, message):
-	try:
-		await message.copy(chat_id=user_id)
-		return 200, None
-	except FloodWait as e:
-		await asyncio.sleep(e.x)
-		return send_msg(user_id, message)
-	except InputUserDeactivated:
-		return 400, f"{user_id} : deactivated\n"
-	except UserIsBlocked:
-		return 400, f"{user_id} : user is blocked\n"
-	except PeerIdInvalid:
-		return 400, f"{user_id} : user id invalid\n"
-	except Exception as e:
-		return 500, f"{user_id} : {traceback.format_exc()}\n"
+    try:
+        await message.copy(chat_id=user_id)
+        return 200, None
+    except FloodWait as e:
+        await asyncio.sleep(e.x)
+        return send_msg(user_id, message)
+    except InputUserDeactivated:
+        return 400, f"{user_id} : deactivated\n"
+    except UserIsBlocked:
+        return 400, f"{user_id} : user is blocked\n"
+    except PeerIdInvalid:
+        return 400, f"{user_id} : user id invalid\n"
+    except Exception as e:
+        return 500, f"{user_id} : {traceback.format_exc()}\n"
 
 
 @Bot.on_callback_query()
@@ -144,12 +146,12 @@ async def cb_handler(bot, update):
 async def start(bot, update):
     
     if not await db.is_user_exist(update.from_user.id):
-	    await db.add_user(update.from_user.id)
+        await db.add_user(update.from_user.id)
     
     await update.reply_text(
         text=START_TEXT.format(update.from_user.mention),
         disable_web_page_preview=True,
-	reply_markup=START_BUTTONS
+        reply_markup=START_BUTTONS
     )
 
 
@@ -157,12 +159,12 @@ async def start(bot, update):
 async def help(bot, update):
     
     if not await db.is_user_exist(update.from_user.id):
-	    await db.add_user(update.from_user.id)
+        await db.add_user(update.from_user.id)
     
     await update.reply_text(
         text=HELP_TEXT,
-      	disable_web_page_preview=True,
-	reply_markup=HELP_BUTTONS
+        disable_web_page_preview=True,
+        reply_markup=HELP_BUTTONS
     )
 
 
@@ -170,12 +172,12 @@ async def help(bot, update):
 async def about(bot, update):
     
     if not await db.is_user_exist(update.from_user.id):
-	    await db.add_user(update.from_user.id)
+        await db.add_user(update.from_user.id)
     
     await update.reply_text(
         text=ABOUT_TEXT.format((await bot.get_me()).username),
         disable_web_page_preview=True,
-	reply_markup=ABOUT_BUTTONS
+        reply_markup=ABOUT_BUTTONS
     )
 
 
@@ -183,7 +185,7 @@ async def about(bot, update):
 async def telegraph_upload(bot, update):
     
     if not await db.is_user_exist(update.from_user.id):
-	    await db.add_user(update.from_user.id)
+        await db.add_user(update.from_user.id)
     
     text = await update.reply_text(
         text="<code>Downloading to My Server ...</code>",
@@ -227,55 +229,69 @@ async def telegraph_upload(bot, update):
     )
 
 
-@Bot.on_message(filters.private & filters.command("broadcast") & filters.user(BOT_OWNER) & filters.reply)
-async def broadcast(bot, update):
-	broadcast_ids = {}
-	all_users = await db.get_all_users()
-	broadcast_msg = update.reply_to_message
-	while True:
-	    broadcast_id = ''.join([random.choice(string.ascii_letters) for i in range(3)])
-	    if not broadcast_ids.get(broadcast_id):
-	        break
-	out = await update.reply_text(text=f"Broadcast Started! You will be notified with log file when all the users are notified.")
-	start_time = time.time()
-	total_users = await db.total_users_count()
-	done = 0
-	failed = 0
-	success = 0
-	broadcast_ids[broadcast_id] = dict(total = total_users, current = done, failed = failed, success = success)
-	async with aiofiles.open('broadcast.txt', 'w') as broadcast_log_file:
-	    async for user in all_users:
-	        sts, msg = await send_msg(user_id = int(user['id']), message = broadcast_msg)
-	        if msg is not None:
-	            await broadcast_log_file.write(msg)
-	        if sts == 200:
-	            success += 1
-	        else:
-	            failed += 1
-	        if sts == 400:
-	            await db.delete_user(user['id'])
-	        done += 1
-	        if broadcast_ids.get(broadcast_id) is None:
-	            break
-	        else:
-	            broadcast_ids[broadcast_id].update(dict(current = done, failed = failed, success = success))
-	if broadcast_ids.get(broadcast_id):
-	    broadcast_ids.pop(broadcast_id)
-	completed_in = datetime.timedelta(seconds=int(time.time()-start_time))
-	await asyncio.sleep(3)
-	await out.delete()
-	if failed == 0:
-	    await update.reply_text(text=f"broadcast completed in `{completed_in}`\n\nTotal users {total_users}.\nTotal done {done}, {success} success and {failed} failed.", quote=True)
-	else:
-	    await update.reply_document(document='broadcast.txt', caption=f"broadcast completed in `{completed_in}`\n\nTotal users {total_users}.\nTotal done {done}, {success} success and {failed} failed.")
-	os.remove('broadcast.txt')
+@Bot.on_message(
+    filters.private &
+    filters.command("broadcast") &
+    filters.user(BOT_OWNER) &
+    filters.reply
+)
+async def broadcast(bot, update, broadcast_ids={}):
+    
+    all_users = await db.get_all_users()
+    broadcast_msg = update.
+    
+    while True:
+        broadcast_id = ''.join([random.choice(string.ascii_letters) for i in range(3)])
+        if not broadcast_ids.get(broadcast_id):
+            break
+
+    out = await update.reply_text(text=f"Broadcast Started! You will be notified with log file when all the users are notified.")
+    start_time = time.time()
+    total_users = await db.total_users_count()
+    done = 0
+    failed = 0
+    success = 0
+    broadcast_ids[broadcast_id] = dict(total = total_users, current = done, failed = failed, success = success)
+        
+    async with aiofiles.open('broadcast.txt', 'w') as broadcast_log_file:
+        async for user in all_users:
+            sts, msg = await send_msg(user_id = int(user['id']), message = broadcast_msg)
+            if msg is not None:
+                await broadcast_log_file.write(msg)
+            if sts == 200:
+                success += 1
+            else:
+                failed += 1
+            if sts == 400:
+                await db.delete_user(user['id'])
+            done += 1
+            if broadcast_ids.get(broadcast_id) is None:
+                break
+            else:
+                broadcast_ids[broadcast_id].update(dict(current = done, failed = failed, success = success))
+        
+    if broadcast_ids.get(broadcast_id):
+        broadcast_ids.pop(broadcast_id)
+    
+    completed_in = datetime.timedelta(seconds=int(time.time()-start_time))
+    await asyncio.sleep(3)
+    await out.delete()
+
+    if failed == 0:
+        await update.reply_text(text=f"broadcast completed in `{completed_in}`\n\nTotal users {total_users}.\nTotal done {done}, {success} success and {failed} failed.", quote=True)
+    else:
+        await update.reply_document(document='broadcast.txt', caption=f"broadcast completed in `{completed_in}`\n\nTotal users {total_users}.\nTotal done {done}, {success} success and {failed} failed.")
+        
+    os.remove('broadcast.txt')
 
 
 @Bot.on_message(filters.private & filters.command("status"), group=5)
 async def status(bot, update):
+
     total_users = await db.total_users_count()
     text = "**Bot Status**\n"
     text += f"\n**Total Users:** `{total_users}`"
+
     await update.reply_text(
         text=text,
         quote=True,
